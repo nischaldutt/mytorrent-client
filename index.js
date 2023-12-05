bd833313c56bcd1018363d1721ff151100cfd943
nischal <nischaldutt01@gmail.com>
2023-12-05 08:16:45 +0530

    build: nodemon, bencode

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 "use strict";
 
-const fs = require("fs");
-const bencode = require("bencode");
+import fs from "fs";
+import bencode from "bencode";
 
 const torrent = bencode.decode(fs.readFileSync("puppy.torrent"));
-console.log(torrent.toString("utf8"));
+console.log(torrent);
