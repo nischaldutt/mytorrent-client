dd0db1ddd4bcfc7c98b71d9626d33ce3bc06d00e
nischal <nischaldutt01@gmail.com>
2023-12-07 16:34:25 +0530

    feat: get peers from tracker url

--- tracker.js
+++ tracker.js
@@ -1,22 +1,43 @@
 "use strict";
 
 import dgram from "dgram";
-import buffer from "buffer";
+import { Buffer } from "buffer";
 import { URL } from "url";
-
-const Buffer = buffer.Buffer;
-const torrentUrlObj = new URL("udp://tracker.coppersurfer.tk:6969/announce");
-
-console.log({ torrentUrlObj });
 
 export const getPeers = (torrent, callback) => {
   const socket = dgram.createSocket("udp4");
   const torrentUrlObj = new URL(torrent.announce.toString("utf8"));
 
   // 1. send connect request
-  // udpSend(socket, buildConnectReq(), torrentUrlObj);
+  udpSend(socket, buildConnectReq(), torrentUrlObj);
+
+  socket.on("message", (response) => {
+    if (respType(response) === "connect") {
+      // 2. receive and parse the connect response
+      const connResp = parseConnectResp(response);
+
+      // 3. send announce request
+      const announceReq = buildAnnounceReq(connResp.connectionId);
+      udpSend(socket, announceReq, torrentUrlObj);
+    } else if (respType(response) === "announce") {
+      // 4. parse announce response
+      const announceResp = parseAnnounceResp(response);
+      // 5. pass peers to callback
+      callback(announceResp.peers);
+    }
+  });
 };
 
 function udpSend(socket, message, urlObj, callback = () => {}) {
   socket.send(message, 0, message.length, urlObj.port, urlObj.host, callback);
 }
+
+function respType(resp) {}
+
+function buildConnectReq() {}
+
+function parseConnectResp(resp) {}
+
+function buildAnnounceReq(connId) {}
+
+function parseAnnounceResp(resp) {}
