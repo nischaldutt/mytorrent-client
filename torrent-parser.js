c3b991497c9ec92dabfe107e25188d57bea278ce
nischal <nischaldutt01@gmail.com>
2023-12-10 18:57:42 +0530

    update: adding infoHash and sie functions to torrent-parser

--- torrent-parser.js
+++ torrent-parser.js
@@ -2,11 +2,24 @@
 
 import fs from "fs";
 import bencode from "bencode";
+import crypto from "crypto";
+import bignum from "bignum";
 
 export function open(filePath) {
   return bencode.decode(fs.readFileSync(filePath));
 }
 
-export function size(torrent) {}
+export function size(torrent) {
+  const size = torrent.info.files
+    ? torrent.info.files
+        .map((file) => file.length)
+        .reduce((acc, curr) => acc + curr)
+    : torrent.info.length;
 
-export function infoHash(torrent) {}
+  return bignum.toBuffer(size, { size: 8 });
+}
+
+export function infoHash(torrent) {
+  const info = bencode.decode(torrent.info);
+  return crypto.createHash("sha1").update(info).digest();
+}
